da5376d4ec3cacc015dc29daf74dcd7bd6d41e1c
Kelley Lynch <kelleylynch@gmail.com>
2023-03-16 16:02:02 -0400

    changing hostname

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 BUILDPROJ=$1
 
-$VPS_HOME/vps/cmake_build/via_project_server 0.0.0.0 9669 8 /data/via_project_server/latest /data/via_project_server/rev /data/via_project_server/revdb /data/via_project_server/log &
+echo "UPDATED VERSION2" # todo refactor into two separate scripts
+mkdir -p /data/via_project_server/latest /data/via_project_server/rev /data/via_project_server/revdb /data/via_project_server/log
+$VPS_HOME/vps/scripts/create_bucket_folders.sh /data/via_project_server/latest # (run only first time) creates folders 00, 01, .. f2, ff
+$VPS_HOME/vps/scripts/create_bucket_folders.sh /data/via_project_server/rev    # (run only first time) creates folders 00, 01, .. f2, ff
+
+$VPS_HOME/vps/cmake_build/via_project_server $HOSTNAME 9669 8 /data/via_project_server/latest /data/via_project_server/rev /data/via_project_server/revdb /data/via_project_server/log &
 sleep 10
 
 # generate json project configuration
@@ -13,10 +18,10 @@ sleep 5
 
 if  [ "$BUILDPROJ" = "build-project" ]
 then
-  PROJ_ID=$(curl -s --data @project_file.json 'http://0.0.0.0:9669' | jq ".pid")
-  echo $PROJ_ID >> /data/proj_id.txt
+  PROJ_ID=$(curl -s --data @project_file.json 'http://${HOSTNAME}:9669' | jq ".pid")
+  echo $PROJ_ID >> data/proj_id.txt
   echo $PROJ_ID
 fi 
 
-gunicorn app:app -w 4 --threads 4 -b 0.0.0.0:9779
+gunicorn app:app -w 4 --threads 4 -b $HOSTNAME:9779
 
